c6d59eb626f4dcf6501933aa754f87bf9c9ce32e
Lionel Henry <lionel.hry@gmail.com>
2018-04-14 18:43:39 +0200

    Provide argument pronouns instead of flipping order

--- R/dispatch.R
+++ R/dispatch.R
@@ -1,8 +1,6 @@
 
 dispatch2 <- function(generic, x, y, env = caller_env(2)) {
-  input_c1 <- class(x)
-  input_c2 <- class(y)
-  classes <- sort.int(c(input_c1, input_c2), method = "radix")
+  classes <- sort.int(c(class(x), class(y)), method = "radix")
   c1 <- classes[[1]]
   c2 <- classes[[2]]
 
@@ -10,20 +8,21 @@ dispatch2 <- function(generic, x, y, env = caller_env(2)) {
     table <- binary_table(env)
 
     if (!is_null(table)) {
-      fn <- table[[generic]][[c1]][[c2]][["mtd"]]
+      fn <- table[[generic]][[c1]][[c2]]
 
       if (!is_null(fn)) {
-        flipped_def <- table[[generic]][[c1]][[c2]][["flipped"]]
-        flipped_args <- input_c1 != c1
-        if (flipped_def + flipped_args == 1L) {
-          fmls <- formals(fn)
-          stopifnot(length(fmls) >= 2L)
-          names(fmls)[1:2] <- names(fmls[2:1])
-          formals(fn) <- fmls
-        }
-
         call <- sys.call(sys.parent(1L))
         frame <- sys.frame(sys.parent(2L))
+
+        frame$.dispatched <- new_list(2L, names = classes)
+        if (c1 == class(x)) {
+          frame$.dispatched[[c1]] <- x
+          frame$.dispatched[[c2]] <- y
+        } else {
+          frame$.dispatched[[c1]] <- y
+          frame$.dispatched[[c2]] <- x
+        }
+
         node_poke_car(call, fn)
         return(eval_bare(call, frame))
       }
@@ -71,12 +70,8 @@ def_method2 <- function(.class1, .class2, ..., .env = caller_env()) {
     } else if (!is_null(mtd[[c1]][[c2]])) {
       warn(sprintf("Overriding method `%s()` for classes `%s` and `%s`", name, c1, c2))
     }
-    if (is_null(mtd[[c1]][[c2]])) {
-      mtd[[c1]][[c2]] <- list()
-    }
 
-    mtd[[c1]][[c2]][["mtd"]] <- methods[[name]]
-    mtd[[c1]][[c2]][["flipped"]] <- c1 != .class1
+    mtd[[c1]][[c2]] <- methods[[name]]
   }
 
   invisible(methods)
--- tests/testthat/test-dispatch.R
+++ tests/testthat/test-dispatch.R
@@ -10,10 +10,10 @@ test_that("def_method2() registers methods in current env", {
 
   table_mtd <- table$mtd
   expect_is(table_mtd, "environment")
-  expect_identical(table_mtd$c1$c2$mtd, m1)
+  expect_identical(table_mtd$c1$c2, m1)
 
   def_method2("c3", "c2", mtd = m2)
-  expect_identical(table_mtd$c2$c3$mtd, m2)
+  expect_identical(table_mtd$c2$c3, m2)
 })
 
 test_that("redefining a method warns", {
@@ -25,8 +25,8 @@ test_that("redefining a method warns", {
   expect_warning(def_method2("c3", "c1", mtd = m2), "`c1` and `c3`")
 
   table <- env_get(, binary_table_name)
-  expect_identical(table$mtd$c1$c2$mtd, m2)
-  expect_identical(table$mtd$c1$c3$mtd, m2)
+  expect_identical(table$mtd$c1$c2, m2)
+  expect_identical(table$mtd$c1$c3, m2)
 })
 
 test_that("dispatch2() finds methods", {
@@ -53,22 +53,15 @@ test_that("dispatch2() passes all arguments", {
   expect_identical(fn(1, 2, 3, z = 4, foo = 5), list(3, foo = 5, x = 1, y = 2, z = 4))
 })
 
-test_that("method arguments are flipped if necessary", {
-  fn <- function(x, y) {
-    dispatch2("fn", x, y)
-  }
+test_that("dispatch2() defines `.dispatched` pronoun", {
+  fn <- function(x, y) dispatch2("fn", x, y)
 
-  def_method2("factor", "character", fn = function(fct, chr) {
-    list(fct = fct, chr = chr)
+  def_method2("character", "integer", fn = function(x, y) {
+    chr <- .dispatched$character
+    int <- .dispatched$integer
+    list(chr = chr, int = int)
   })
-  exp <- list(fct = factor("foo"), chr = "foo")
-  expect_identical(fn("foo", factor("foo")), exp)
 
-  # Should produce warning:
-  expect_warning(regex = "Overriding",
-    def_method2("character", "factor", fn = function(chr, fct) {
-      list(fct = fct, chr = chr)
-    })
-  )
-  expect_identical(fn("foo", factor("foo")), exp)
+  expect_identical(fn("foo", 1L), list(chr = "foo", int = 1L))
+  expect_identical(fn(1L, "foo"), list(chr = "foo", int = 1L))
 })
